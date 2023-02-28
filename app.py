43dfa44e1ea1a1dfa9ff10c9698d46d9186ca701
obaidsk7865 <125446316+obaidsk7865@users.noreply.github.com>
2023-02-28 16:48:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.ensemble import RandomForestRegressor
 
 model = pickle.load(open('RandomForestRegressor.pkl', 'rb'))
 df = pickle.load(open('df.pkl', 'rb'))
