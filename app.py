5d42f4ad074d2304932800a83f7aee2f1cd656bd
obaidsk7865 <125446316+obaidsk7865@users.noreply.github.com>
2023-02-28 16:41:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+model = pickle.load(open('RandomForestRegressor.pkl', 'rb'))
+df = pickle.load(open('df.pkl', 'rb'))
+
+
+st.title('Gold_Price_Prediction')
+
+SPX = st.number_input('SPX:')
+USO = st.number_input('USO:')
+SLV = st.number_input('SLV:')
+EUR_USD = st.number_input('EUR/USD:')
+
+prediction=''
+if st.button('Predict'):
+    input = pd.DataFrame([[SPX, USO, SLV, EUR_USD]], columns=['SPX', 'USO', 'SLV', 'EUR/USD'])
+    prediction += str(model.predict(input)[0])
+
+st.success(prediction)
